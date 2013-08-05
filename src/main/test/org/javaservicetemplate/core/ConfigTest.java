9d6323e567ae85f137d5d3116f3e84dbb99b9d31
Eduardo Bohrer <nbluisrs@gmail.com>
2013-08-05 13:39:30 -0300

    renaming configuration class

--- src/main/test/org/javaservicetemplate/core/ConfigTest.java
+++ src/main/test/org/javaservicetemplate/core/ConfigTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ConfigurationTest {
+public class ConfigTest {
 
 	private Config configuration;
 
