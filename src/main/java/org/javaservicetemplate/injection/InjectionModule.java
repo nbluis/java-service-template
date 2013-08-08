5f8309f7f299d3b36f2ad61064984ce4d0c9e9a3
Eduardo Bohrer <nbluisrs@gmail.com>
2013-08-08 14:55:23 -0300

    registering transactioninterceptor in injectionmodule

--- src/main/java/org/javaservicetemplate/injection/InjectionModule.java
+++ src/main/java/org/javaservicetemplate/injection/InjectionModule.java
@@ -1,14 +1,21 @@
 package org.javaservicetemplate.injection;
 
 import org.hibernate.SessionFactory;
+import org.javaservicetemplate.dao.AbstractDao;
+import org.javaservicetemplate.interceptor.TransactionInterceptor;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.matcher.Matchers;
 
 public class InjectionModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
 		bind(SessionFactory.class).toProvider(SessionFactoryProvider.class);
+
+		TransactionInterceptor interceptor = new TransactionInterceptor();
+		requestInjection(interceptor);
+		bindInterceptor(Matchers.subclassesOf(AbstractDao.class), Matchers.any(), interceptor);
 	}
 
 }
